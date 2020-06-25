527b418faa7af7a2d5176135ead64a790fc1ec0d
ing <inyoman_gurnitha@yahoo.com>
2020-06-25 21:12:13 +0700

    8.42. Creating Our First Model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+class AdminPost(admin.ModelAdmin):
+
+	list_filter 	= ['publishing_date'] 	
+	list_display 	= ['title', 'publishing_date']
+	searh_fields 	= ['title', 'contend']
+
+admin.site.register(Post, AdminPost)	
