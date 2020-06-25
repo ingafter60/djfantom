df11289f6c003112cead16ddb6c72380619c04c9
ing <inyoman_gurnitha@yahoo.com>
2020-06-25 22:11:47 +0700

    11.45. How To Use Generic Class Based View (DetailView) to Load and Display single post on singlepage

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
 	path('', IndexView.as_view(), name="index"),
+	path('detail/<int:pk>', PostDetail.as_view(), name="detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,11 @@
 # posts/views.py
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Post 
 
 # Create your views here.
 
-# class IndexView(TemplateView):
+# ALL POSTS
 class IndexView(ListView):
 
 	template_name 	= "posts/index.html"
@@ -14,4 +14,15 @@ class IndexView(ListView):
 
 	def get_context_data(self, *, object_list=None, **kwargs):
 		context = super(IndexView, self).get_context_data(**kwargs)
+		return context
+
+# SINGLE POST
+class PostDetail(DetailView):
+
+	template_name 	= "posts/detail.html"
+	model = Post
+	context_object_name = 'single'
+
+	def get_context_data(self, **kwargs):
+		context = super(PostDetail, self).get_context_data(**kwargs)
 		return context
