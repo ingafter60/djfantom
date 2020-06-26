c9bfe4859dab935346d6dace2a0988b35b392137
ing <inyoman_gurnitha@yahoo.com>
2020-06-26 16:34:10 +0700

    20.53. How To Create Tag Model

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 # Register your models here.
 
@@ -11,3 +11,4 @@ class AdminPost(admin.ModelAdmin):
 
 admin.site.register(Post, AdminPost)	
 admin.site.register(Category)
+admin.site.register(Tag)
--- posts/models.py
+++ posts/models.py
@@ -24,6 +24,21 @@ class Category(models.Model):
 		return self.posts.all().count() 
 
 
+# TAG MODELS/TABLE
+class Tag(models.Model):
+
+	title = models.CharField(max_length=50)
+	slug  = models.SlugField(editable=False)
+
+	def __str__(self):
+		return self.title
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super(Tag, self).save(*args, **kwargs)	
+
+
+
 # POST MODELS/TABLE
 class Post(models.Model):
 
@@ -38,6 +53,7 @@ class Post(models.Model):
 					on_delete=models.CASCADE, 
 					default=1,
 					related_name="posts")
+	tag 		= models.ManyToManyField(Tag, related_name="posts", blank=True)
 	
 	# Define method to automatically save title as slug
 	def save(self, *args, **kwargs):
