23fcdff5d3227382ea05ec8ef2aa76432d660040
ing <inyoman_gurnitha@yahoo.com>
2020-06-26 08:41:24 +0700

    14.47. How To Use Slug Field

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
@@ -10,3 +11,8 @@ class Post(models.Model):
 	publishing_date = models.DateTimeField(auto_now_add=True)
 	image 	= models.ImageField(blank=True, null=True, upload_to="uploads/")
 	user 		= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	slug 		= models.SlugField(default='slug')
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super(Post, self).save(*args, **kwargs)
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
+# posts/urls.py
 from django.urls import path 
 from .views import *
 
 urlpatterns = [
 	path('', IndexView.as_view(), name="index"),
-	path('detail/<int:pk>', PostDetail.as_view(), name="detail"),
+	# path('detail/<int:pk>', PostDetail.as_view(), name="detail"),
+	path('detail/<int:pk>/<slug:slug>', PostDetail.as_view(), name="detail"),
 ]
